5a21c354a0784b66118b3845614cd4a72313f09e
19StRoNGeR98 <admin@kvantoriumpskov.ru>
2024-03-13 18:30:13 +0300

    VR

---
+++ data.js
@@ -0,0 +1,239 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "ага",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9016894395662085,
+          "pitch": -0.12745619885911097,
+          "rotation": 0,
+          "target": "1-_20240306175234"
+        },
+        {
+          "yaw": 0.986927839349967,
+          "pitch": -0.0011273985170632272,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.7434628139652055,
+          "pitch": -0.09289736811183502,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2684039948868353,
+          "pitch": -0.871117608688845,
+          "title": "Кто это",
+          "text": "Лайк"
+        }
+      ]
+    },
+    {
+      "id": "1-_20240306175234",
+      "name": "кр_20240306175234",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.713529777294001,
+        "pitch": 0.30146904223692417,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0730040314863967,
+          "pitch": 0.1996629961707761,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.634811228373092,
+          "pitch": 0.11818807094993211,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.1737407888934719,
+          "pitch": -0.21016857498740826,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.06043772384228774,
+          "pitch": 0.03708993972179897,
+          "title": "Матвей",
+          "text": "Это он"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "выпфа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8935054158739977,
+          "pitch": -0.009066899803611861,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.05323381192587,
+          "pitch": 0.21495281989545667,
+          "rotation": 0,
+          "target": "1-_20240306175234"
+        },
+        {
+          "yaw": -0.33210382212046774,
+          "pitch": 0.02468615727179113,
+          "rotation": 0,
+          "target": "1-_20240306175234"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9626681846766694,
+          "pitch": 0.055633209570197906,
+          "title": "Галерея",
+          "text": "Галерея"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "мда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.724500407409872,
+          "pitch": 0.18630702550203893,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.8027632532248274,
+          "pitch": 0.09245261276079475,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.6310439598040034,
+          "pitch": 0.108395689365679,
+          "rotation": 0,
+          "target": "1-_20240306175234"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8990569583116148,
+          "pitch": 0.18590556883276754,
+          "title": "Марк",
+          "text": "Это я"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
